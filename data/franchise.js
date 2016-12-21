27a0415a726ef20695b83d9b91d1a30581e9ad91
AxelPAL <axelpal@gmail.com>
2016-12-21 15:56:03 +0500

    T4371: city field

--- data/franchise.js
+++ data/franchise.js
@@ -4,17 +4,20 @@ $(document).ready(function() {
     $('.center button').on('click', function() {
         $('.modal_bg').show();
         $('.modal_form').show();
+        $('body').css({overflow: 'hidden'});
     });
     $('.closeModal').on('click', function(event) {
         event.preventDefault(event);
         $('.modal_bg').hide();
         $('.modal_form').hide();
         $('.modal_message').hide();
+        $('body').css({overflow: 'auto'});
     });
     $('.modal_bg').on('click', function(event) {
         $('.modal_bg').hide();
         $('.modal_form').hide();
         $('.modal_message').hide();
+        $('body').css({overflow: 'auto'});
     });
     $('#form').on('submit', function(event) {
         event.preventDefault(event);
